bb64d001931ec61bcde8b95e9c5729b704007561
wa130 <nasyuwa648@gmail.com>
2026-03-02 17:49:52 +0000

    belajar membuat command

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ try {
   
   Nass.onText(/\/start/, (msg) => {
     Nass.sendMessage(msg.chat.id, `halo coy`)
+
+    Nass.on("message", (msg) => {
+      console.log('--------- [ PESAN ] --------')
+      const username = msg.from.username
+      const text = msg.text
+
+      console.log(`Dari  : ${username}`)
+      console.log(`Pesan : ${text}`)
+      console.log('---------------------------')
+
+      switch (text) {
+        case "menu":
+          Nass.sendMessage(msg.chat.id, "halo bang")
+          break;
+      
+        default:
+          break;
+      }
+
+    })
   })
 } catch (error) {
   console.log("error saat menjalankan bot")
